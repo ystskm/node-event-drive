d75489ac1b8447939ec211a8e163bd08eea2bdb4
ystskm <brilliantpenguin@gmail.com>
2013-11-20 09:19:00 +0900

    accept outer emitter, v1.0.1

--- index.js
+++ index.js
@@ -3,15 +3,22 @@
 var Emitter = require('events').EventEmitter, micropipe = require('micro-pipe');
 module.exports = eventDrive;
 
-function eventDrive(line, callback) {
-  var ee = new Emitter();
+function eventDrive(emitter, line, callback) {
+
+  if(typeof emitter == 'function' || Array.isArray(emitter))
+    callback = line, line = emitter, emitter = null;
+
+  var ee = emitter || new Emitter();
   if(typeof callback == 'function')
     ee.once('error', callback).once('end', function() {
       var args = Array.prototype.slice.call(arguments);
       callback.apply(null, [null].concat(args));
     });
+
   setImmediate(function() {
     micropipe(line);
   });
+
   return ee;
+
 }
