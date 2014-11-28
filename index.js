746df795cf1d7e6c8786c54cfbaf10bf24983c3b
ystskm <brilliantpenguin@gmail.com>
2014-11-28 19:56:46 +0900

    care leak, v1.1.2

--- index.js
+++ index.js
@@ -4,19 +4,38 @@ var Emitter = require('events').EventEmitter, micropipe = require('micro-pipe');
 var processor = typeof setImmediate == 'function' ? setImmediate: function(fn) {
   process.nextTick(fn);
 };
-module.exports = eventDrive;
 
+module.exports = eventDrive;
 function eventDrive(emitter, line, callback) {
 
   if(!(emitter instanceof Emitter) && typeof callback == 'undefined')
     callback = line, line = emitter, emitter = null;
 
   var ee = emitter || new Emitter();
-  if(typeof callback == 'function')
-    ee.once('error', callback).once('end', function() {
-      var args = Array.prototype.slice.call(arguments);
-      callback.apply(null, [null].concat(args));
+  callback = typeof callback == 'function' ? callback: null;
+
+  var careLeak = function() {
+    setImmediate(function() {
+      ee.removeAllListeners(), ee = null;
     });
+  };
+
+  ee.once('error', function(e) {
+
+    careLeak();
+    if(callback == null)
+      return;
+
+    callback(e);
+  }).once('end', function() {
+
+    careLeak();
+    if(callback == null)
+      return;
+
+    var args = Array.prototype.slice.call(arguments);
+    callback.apply(null, [null].concat(args));
+  });
 
   line == null ? ee.once('trigger', asyncTrigger): asyncTrigger(line);
   return ee;
